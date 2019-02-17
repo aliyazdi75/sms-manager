7abd3f8cfcd966e2810b239f74115a90becdd2a9
Ali Yazdi <ma.yazdi75@gmail.com>
2019-02-18 00:32:11 +0330

    add and config FOSRestBundle and NelmioCorsBundle and JMSSerializerBundle for REST Api

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,10 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    new FOS\RestBundle\FOSRestBundle(),
+    new JMS\SerializerBundle\JMSSerializerBundle(),
+    new Nelmio\CorsBundle\NelmioCorsBundle(),
 ];
