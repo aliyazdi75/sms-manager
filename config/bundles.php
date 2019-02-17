1117fb5e452e39e1b5ea523288282ddffd7482f1
Ali Yazdi <ma.yazdi75@gmail.com>
2019-02-18 03:23:22 +0330

    add doctrine and config database and add entity for sms with number and body

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,9 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    new FOS\RestBundle\FOSRestBundle(),
-    new JMS\SerializerBundle\JMSSerializerBundle(),
-    new Nelmio\CorsBundle\NelmioCorsBundle(),
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
